59136cb5f38cb560fb1eb41cae32de9a455ab00c
unknown <Jordanf@jordanf3.redmond.corp.microsoft.com>
2013-08-20 23:06:12 -0700

    add attendance area

---
+++ NhlStats/Areas/Attendance/Controllers/NhlController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using NhlStatsQuery;
+
+namespace NhlStats.Areas.Attendance.Controllers
+{
+    public class NhlController : Controller
+    {
+        public ActionResult Index(bool update=false)
+        {
+            if (update == true)
+            {
+                ViewBag.GetLatest = true;
+            }
+
+            string season = (string)Request.Form["SeasonList"];
+            ViewBag.Season = season;
+
+            if (String.IsNullOrEmpty(season))
+            {
+                ViewBag.Table = String.Empty;
+            }
+            else
+            {
+                bool performUpdate = false;
+
+                if (this.Request["Update"] == "Update")
+                {
+                    performUpdate = true;
+                }
+
+                ViewBag.Table = NhlStatsQuery.NhlStatsQuery.GetStatsFromDb(season, performUpdate);
+                ViewBag.PerformUpdate = false;
+            }
+
+            return View();
+        }
+
+        public ActionResult Update()
+        {
+            return RedirectToAction("Index", "Nhl", new { update = true });
+        }
+
+    }
+}
--- NhlStats/Controllers/HomeController.cs
+++ NhlStats/Controllers/HomeController.cs
@@ -10,33 +10,9 @@ namespace NhlStats.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index(bool GetLatest=false)
+        public ActionResult Index()
         {
-            //if (this.HttpContext.Session["GetLatest"] == "true")
-            if (GetLatest == true)
-            {
-                ViewBag.GetLatest = true;
-            }
-
-            string season = (string)Request.Form["SeasonList"];
-            ViewBag.Season = season;
-
-            if (String.IsNullOrEmpty(season))
-            {
-                ViewBag.Table = String.Empty;
-            }
-            else
-            {
-                bool performUpdate = false;
-
-                if (this.Request["Update"] == "Update")
-                {
-                    performUpdate = true;
-                }
-
-                ViewBag.Table = NhlStatsQuery.NhlStatsQuery.GetStatsFromDb(season, performUpdate);
-                ViewBag.PerformUpdate = false;
-            }
+            ViewBag.Message = String.Empty;
 
             return View();
         }
