46932e42d140f0aeacf4fea6bac4eeafe57c8236
Jordan Fitzgibbon <jordanfitzgibbon@hotmail.com>
2014-04-30 23:47:47 -0700

    demographics: get page and pull out csv values

--- NhlStatsQuery/Demographics/DemographicsQuery.cs
+++ NhlStatsQuery/Demographics/DemographicsQuery.cs
@@ -4,6 +4,7 @@ using System.Data.Entity;
 using System.Globalization;
 using System.Linq;
 using System.Net.Http;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 using HtmlAgilityPack;
@@ -13,8 +14,8 @@ namespace SportsData.Demographics
 {
     public abstract class DemographicsQuery
     {
-        protected string BaseAddress = "http://zipwho.com/";
-        protected string PageFormatString = "?zip={0}&mode=zip";
+        protected static string BaseAddress = "http://zipwho.com/";
+        protected static string RelativeUrlFormatString = "?zip={0}&mode=zip";
 
         public static List<DemographicsModel> GetDemographics(List<int> zipCodes)
         {
@@ -33,13 +34,51 @@ namespace SportsData.Demographics
             return results;
         }
 
-        public static DemographicsModel GetDemographic(int zipCodes)
+        public static DemographicsModel GetDemographic(int zipCode)
         {
-            DemographicsModel result = new DemographicsModel();
-
-
+            string page = DemographicsQuery.GetPage(zipCode);
+            DemographicsModel result = DemographicsQuery.ParsePage(page);
             return result;
         }
 
+        private static string GetPage(int zipCode)
+        {
+            string relativeUrlString = String.Format(DemographicsQuery.RelativeUrlFormatString, zipCode);
+            Uri relativeUrl = new Uri(relativeUrlString, UriKind.Relative);
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(DemographicsQuery.BaseAddress);
+
+            Task<string> httpResponseMessage = httpClient.GetStringAsync(relativeUrl);
+            string responseString = httpResponseMessage.Result;
+            return responseString;
+        }
+
+        private static DemographicsModel ParsePage(string page)
+        {
+            // Get the csv list of fields and values. This needs to be done in two steps until a better regex can be made.
+            string pattern = @"getData\(\)(?<text>.*?)"";";
+            Regex regex = new Regex(pattern, RegexOptions.Singleline);
+            Match match = regex.Match(page);
+            string partialText = match.Groups["text"].Value;
+
+            pattern = @"""(?<fields>.*?)\\n(?<values>.*?)$";
+            regex = new Regex(pattern, RegexOptions.Singleline);
+            match = regex.Match(partialText);
+            string fieldsCsv = match.Groups["fields"].Value;
+            string valuesCsv = match.Groups["values"].Value;
+
+            string[] fields = fieldsCsv.Split(',');
+            string[] values = valuesCsv.Split(',');
+            
+            // Fields and values should align by their index. Put them into a dictionary.
+            Dictionary<string, string> data = new Dictionary<string, string>();
+            for (int i=0; i<fields.Length; i++)
+            {
+                data.Add(fields[i], values[i]);
+            }
+
+            return null;
+        }
     }
 }
---
+++ SportsDataTests/Demographics/DemographicsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using SportsData.Demographics;
+using SportsData.Models;
+
+namespace SportsDataTests
+{
+    [TestClass]
+    public class DemographicsTests : SportsDataTestsBaseClass
+    {
+        [TestMethod]
+        public void DemographicsTest()
+        {
+            List<int> zipCodes = new List<int> {98103};
+
+            List<DemographicsModel> results = DemographicsQuery.GetDemographics(zipCodes);
+
+            Assert.AreEqual(1, results.Count);
+        }
+    }
+}
