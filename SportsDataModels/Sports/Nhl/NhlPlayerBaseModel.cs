79c7638c1ebca9cad2eb57f3f83cb493161d9a5f
unknown <Jordanf@jordanf3.redmond.corp.microsoft.com>
2014-06-08 22:55:40 -0700

    add nhl player stats model

---
+++ SportsDataModels/Sports/Nhl/NhlPlayerBaseModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsData.Models
+{
+    public class NhlPlayerStatsBaseModel
+    {
+        [Key, Column(Order = 0)]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        public NhlSeasonType NhlSeasonType { get; set; }
+
+        /// <summary>
+        /// Use the later year as the year. Eg. use 2014 for the 2013-2014 season.
+        /// </summary>
+        public int Year { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Position { get; set; }
+
+        [Required]
+        public string Team { get; set; }
+
+    }
+}
---
+++ SportsDataModels/Sports/Nhl/NhlPlayerStatsBioModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportsData.Models
+{
+    [Table("NhlPlayerStatsBioModel")]
+    public class NhlPlayerStatsBioModel : NhlPlayerStatsBaseModel
+    {
+        public DateTime DateOfBirth { get; set; }
+
+        public string BirthCity { get; set; }
+
+        public string StateOrProvince { get; set; }
+
+        public string BirthCountry { get; set; }
+
+        public int Height { get; set; }
+
+        public int Weight { get; set; }
+
+        public string Shoots { get; set; }
+
+        public int DraftYear { get; set; }
+
+        public int DraftRound { get; set; }
+
+        public int DraftOverall { get; set; }
+
+        public bool Rookie { get; set; }
+
+        public int GamesPlayed { get; set; }
+
+        public int Goals { get; set; }
+
+        public int Assists { get; set; }
+
+        public int Points { get; set; }
+
+        public int PlusMinus { get; set; }
+
+        public int PIM { get; set; }
+
+        public int ToiSecondsPerGame { get; set; }
+
+    }
+}
